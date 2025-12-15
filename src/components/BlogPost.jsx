0e4aa96b683607157d405b840db2f0651a6f73d8
waqas ali <97301126+Wikibro@users.noreply.github.com>
2025-12-15 11:52:42 +0500

    Remove duplicate imports in BlogPost.jsx

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -295,7 +295,7 @@
 // //     </div>
 // //   );
 // // }
-
+import { useParams, Link } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
